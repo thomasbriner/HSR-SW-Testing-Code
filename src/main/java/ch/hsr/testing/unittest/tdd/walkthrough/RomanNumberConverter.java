6b2fb4bd5676579d9af9143511885032630b8f95
Thomas Briner <thomas.briner@gmail.com>
2020-09-26 17:12:45 +0200

    green: 1 -> I

--- src/main/java/ch/hsr/testing/unittest/tdd/walkthrough/RomanNumberConverter.java
+++ src/main/java/ch/hsr/testing/unittest/tdd/walkthrough/RomanNumberConverter.java
@@ -2,6 +2,6 @@ package ch.hsr.testing.unittest.tdd.walkthrough;
 
 public class RomanNumberConverter {
     public static String arabic2Roman(int arabic){
-        return "";
+        return "I";
     }
 }
