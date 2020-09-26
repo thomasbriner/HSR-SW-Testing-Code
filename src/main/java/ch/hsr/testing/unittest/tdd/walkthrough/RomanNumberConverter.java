1bfdc10e106d608164ecc98fb611d938e9f4d725
Thomas Briner <thomas.briner@gmail.com>
2020-09-26 19:28:59 +0200

    green: 5 -> V

--- src/main/java/ch/hsr/testing/unittest/tdd/walkthrough/RomanNumberConverter.java
+++ src/main/java/ch/hsr/testing/unittest/tdd/walkthrough/RomanNumberConverter.java
@@ -1,11 +1,50 @@
 package ch.hsr.testing.unittest.tdd.walkthrough;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RomanNumberConverter {
-    public static String arabic2Roman(int arabic) {
+
+    private static List<RomanFigure> romanFigures = Arrays.asList(
+            RomanFigure.I,
+            RomanFigure.V,
+            RomanFigure.X,
+            RomanFigure.L
+    );
+
+    public static String arabic2Roman(int arabicNumber) {
+        int index = getHighestFigureSmallerThan(arabicNumber);
         String roman = "";
-        for (int i = 0; i < arabic; i++) {
-            roman += "I";
+        for (int i = 0; i < arabicNumber; i = i + romanFigures.get(index).getArabicNumber()) {
+            roman += romanFigures.get(index).toString();
         }
         return roman;
+    }
+
+    private static int getHighestFigureSmallerThan(int arabicNumber) {
+        for (int i = 0; i < romanFigures.size(); i++) {
+            if (romanFigures.get(i).getArabicNumber() > arabicNumber) {
+                return i - 1;
+            }
+        }
+        return 0;
+    }
+}
+
+
+enum RomanFigure {
+    I(1),
+    V(5),
+    X(10),
+    L(50);
+
+    private int arabicNumber;
+
+    RomanFigure(int arabicNumber) {
+        this.arabicNumber = arabicNumber;
+    }
+
+    public int getArabicNumber() {
+        return arabicNumber;
     }
 }
