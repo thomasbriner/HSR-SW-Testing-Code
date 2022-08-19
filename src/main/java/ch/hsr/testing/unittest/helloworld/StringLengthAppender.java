c02bb72360811c0efbbc9a5b72affea92482c209
Thomas Briner <thomas.briner@ergon.ch>
2022-08-19 11:16:51 +0200

    add cut for first unit test

---
+++ src/main/java/ch/hsr/testing/unittest/helloworld/StringLengthAppender.java
@@ -0,0 +1,9 @@
+package ch.hsr.testing.unittest.helloworld;
+
+public class StringLengthAppender {
+
+    public String append(String input) {
+        return input + "(Length of string: " + input.length() + " characters)";
+    }
+
+}
