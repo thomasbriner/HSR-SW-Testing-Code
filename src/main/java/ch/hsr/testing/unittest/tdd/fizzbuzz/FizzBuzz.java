3066dccd6199dd1e7fe96accf47c85ca6fd261ee
Thomas Briner <thomas.briner@ergon.ch>
2022-09-21 08:33:32 +0200

    refactor: normale Zahl

--- src/main/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,11 @@ package ch.hsr.testing.unittest.tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+
     public static String processNumber(int number) {
         if (number % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         } else {
             return "";
         }
--- src/test/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,25 +1,28 @@
 package ch.hsr.testing.unittest.tdd.fizzbuzz;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 class FizzBuzzTest {
 
-    @Test
-    void normaleZahl() {
-        String actual = FizzBuzz.processNumber(4);
 
-        Assertions.assertThat(actual).isEqualTo("");
+    @ParameterizedTest(name="Test {index}: {0} -> {1} ({2} ")
+    @MethodSource("getTestInputData")
+    void testDataDriven(int input, String fizzbuzz, String reason){
+        String actual = FizzBuzz.processNumber(input);
+
+        Assertions.assertThat(actual).isEqualTo(fizzbuzz);
     }
 
-
-    @Test
-    void dreierReihe() {
-        String actual = FizzBuzz.processNumber(6);
-
-        Assertions.assertThat(actual).isEqualTo("Fizz");
+    public static Stream<Arguments> getTestInputData() {
+        return Stream.of(
+                Arguments.of(4,"","normal number"),
+                Arguments.of(6,FizzBuzz.FIZZ,"dreierreihe")
+        );
     }
 
 }
