4d2786da74d28f1c3102aa1fee2e3805d854a13a
Thomas Briner <thomas.briner@ergon.ch>
2022-09-21 08:26:48 +0200

    red: normale Zahl

--- src/test/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzzTest.java
@@ -8,9 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     @Test
+    void normaleZahl() {
+        String actual = FizzBuzz.processNumber(4);
+
+        Assertions.assertThat(actual).isEqualTo("");
+    }
+
+
+    @Test
     void dreierReihe() {
         String actual = FizzBuzz.processNumber(6);
 
         Assertions.assertThat(actual).isEqualTo("Fizz");
     }
+
 }
