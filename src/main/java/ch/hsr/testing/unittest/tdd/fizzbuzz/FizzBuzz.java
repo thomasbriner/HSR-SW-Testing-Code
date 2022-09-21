a4855e2236b4ad25ef8cc99d3276f753d7a02ab9
Thomas Briner <thomas.briner@ergon.ch>
2022-09-21 08:23:27 +0200

    red: 3er Reihe

---
+++ src/main/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package ch.hsr.testing.unittest.tdd.fizzbuzz;
+
+public class FizzBuzz {
+
+    public static String processNumber(int number){
+        return "";
+    }
+}
---
+++ src/test/java/ch/hsr/testing/unittest/tdd/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package ch.hsr.testing.unittest.tdd.fizzbuzz;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void dreierReihe() {
+        String actual = FizzBuzz.processNumber(6);
+
+        Assertions.assertThat(actual).isEqualTo("Fizz");
+    }
+}
