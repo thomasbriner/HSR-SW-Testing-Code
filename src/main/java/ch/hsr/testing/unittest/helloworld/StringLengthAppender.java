4638b4acdf2a50c6eecf94924f518ab6289eb1db
Thomas Briner <thomas.briner@ergon.ch>
2023-08-18 12:25:40 +0200

    modify cut for helloworld

--- src/main/java/ch/hsr/testing/unittest/helloworld/StringLengthAppender.java
+++
@@ -1,9 +0,0 @@
-package ch.hsr.testing.unittest.helloworld;
-
-public class StringLengthAppender {
-
-    public String append(String input) {
-        return input + "(Length of string: " + input.length() + " characters)";
-    }
-
-}
