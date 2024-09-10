f708f6d0a44b811149c54b1f64571b69185a85af
Thomas Briner <thomas.briner@ergon.ch>
2024-09-10 09:16:19 +0200

    make compatible for java 11

--- src/main/java/ch/hsr/testing/unittest/helloworld/Quicksorter.java
+++ src/main/java/ch/hsr/testing/unittest/helloworld/Quicksorter.java
@@ -3,15 +3,17 @@ package ch.hsr.testing.unittest.helloworld;
 import java.util.ArrayList;
 import java.util.List;
 
-// https://gist.github.com/ntruessel/bf117eb7679487d87196aeba8077d27f
+import static java.util.stream.Collectors.toList;
+
+// https://gist.github.com/thomasbriner/d3ea976ac116993719da6a6e63ebe8c2
 public class Quicksorter {
     public <T extends Comparable<T>> List<T> quicksort(List<T> list) {
         if (list.size() > 1) {
             var pivot = list.get(list.size() - 1);
 
-            var left = list.stream().filter(it -> it.compareTo(pivot) < 0).toList();
-            var middle = list.stream().filter(it -> it.compareTo(pivot) == 0).toList();
-            var right = list.stream().filter(it -> it.compareTo(pivot) > 0).toList();
+            var left = list.stream().filter(it -> it.compareTo(pivot) < 0).collect(toList());
+            var middle = list.stream().filter(it -> it.compareTo(pivot) == 0).collect(toList());
+            var right = list.stream().filter(it -> it.compareTo(pivot) > 0).collect(toList());
 
             var result = new ArrayList<>(quicksort(left));
             result.addAll(middle);
